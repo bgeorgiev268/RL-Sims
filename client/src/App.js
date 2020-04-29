6f4fb575dc0de105ceab897447491700be3069e9
CandyMan <c.mattson318@gmail.com>
2020-04-28 21:18:57 -0700

    removed P element when logged in from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -57,9 +57,7 @@ class App extends Component {
         <div className="App">
         <Navbar updateUser={this.updateUser} loggedIn={this.state.loggedIn} />
         {/* greet user if logged in: */}
-        {this.state.loggedIn &&
-          <p>Join the party, {this.state.username}!</p>
-        }
+        {this.state.loggedIn }
         {/* Routes to different components */}
           <Route exact path="/" component={Main} />
           <Route path="/login" render={() =>
