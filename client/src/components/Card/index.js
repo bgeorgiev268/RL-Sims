e2685fe4ccb48410725e3e82ca1ea983dd92ff3b
CandyMan <c.mattson318@gmail.com>
2020-05-02 12:19:34 -0700

    minor style tweaks

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -8,7 +8,7 @@ const {name, image, location, star, url} = props;
   <div className="card hoverable z-depth-4 center-align">
     <div className="card-image">
       <img src={image} alt={name} />
-       <button className="btn-floating btn-small waves-effect waves-light blue" id="btn"><i class="material-icons right">add</i></button>
+       <button className="btn-floating btn-small waves-effect waves-light blue" id="btn"><i className="material-icons right">add</i></button>
     </div>
     <div className="card-content">
     <a rel="noopener noreferrer" href={url} target="_blank" className="card-title">{name}</a> 
