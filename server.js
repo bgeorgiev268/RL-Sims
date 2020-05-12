a8bbbbdf346965760f79884fabf0fdaa5dca2c33
zuk881 <zuk881@cox.net>
2020-05-11 20:39:56 -0700

    heroku deploy attempt

--- server.js
+++ server.js
@@ -29,6 +29,21 @@ app.use(
 	})
 )
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://dude:user123@ds145694.mlab.com:45694/heroku_1wq7p7nc', {
+useNewUrlParser: true,
+useUnifiedTopology: true
+});
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.use(express.static(path.join(__dirname, 'build')));
+-app.get('/', function (req, res) {
++app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
+
 // Passport
 app.use(passport.initialize())
 app.use(passport.session()) // calls the deserializeUser
